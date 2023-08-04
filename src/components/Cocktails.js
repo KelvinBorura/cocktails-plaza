228ce15f7aa39a21b95a0285a536c055b9a6459c
Crystal <muthonigachanja2000@gmail.com>
2023-08-04 10:56:52 -0700

    Adding the imports

--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
+import Error from "./Error";
 
 function Cocktails() {
   return (
