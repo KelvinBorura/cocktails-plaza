b20627547c2050324bc8a1442b47c5ffdcd45208
Crystal <muthonigachanja2000@gmail.com>
2023-08-04 11:58:29 -0700

    Add useState and useEffect in imports

--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { Link } from "react-router-dom";
