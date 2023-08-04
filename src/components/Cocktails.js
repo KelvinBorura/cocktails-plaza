a46b098f8da95a34b01415611d0f69b6ad2f4929
Crystal <muthonigachanja2000@gmail.com>
2023-08-04 11:01:00 -0700

    Using the useState, wrapping the axios.get()function

--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -5,10 +5,38 @@ import { Link } from "react-router-dom";
 import LoadingSpinner from "./LoadingSpinner";
 import Error from "./Error";
 
-function Cocktails() {
-  return (
-    <div>Cocktails</div>
-  )
+const Cocktails = () => {
+    const [query, setQuery] = useState("");
+    const [drinks, setDrinks] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const searchDrinks = async () => {
+        setIsLoading(true);
+        setError(null);
+  
+        try {
+          const response = await axios.get(
+            `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
+          );
+          setDrinks(response.data.drinks);
+        } catch (e) {
+          setError("Failed to fetch drinks!");
+        }
+  
+        setIsLoading(false);
+      };
+  
+      if (query && query.trim()) {
+        searchDrinks();
+      }
+    }, [query]);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setQuery(e.target.elements.search.value);
+    };
 }
 
 export default Cocktails
