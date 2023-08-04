00db03d713037a3d2f49d200f7170f164843bf78
Crystal <muthonigachanja2000@gmail.com>
2023-08-04 12:04:27 -0700

    Create LoadingSpinner.js and Error.js

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
+import Error from "./Error";
 
 
 function Cocktail() {
