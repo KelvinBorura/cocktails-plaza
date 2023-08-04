76f8191fa7b9412bc41e8f76a901532fb7ddf882
Crystal <muthonigachanja2000@gmail.com>
2023-08-04 12:07:37 -0700

    using the useState and await axios

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -5,8 +5,43 @@ import { Link } from "react-router-dom";
 import LoadingSpinner from "./LoadingSpinner";
 import Error from "./Error";
 
-
-function Cocktail() {
+const Cocktail = () => {
+      const { id } = useParams();
+      const [drink, setDrink] = useState(null);
+      const [isLoading, setIsLoading] = useState(false);
+      const [error, setError] = useState(null);
+    
+      useEffect(() => {
+        const getDrink = async () => {
+          setIsLoading(true);
+          setError(null);
+    
+          try {
+            const response = await axios.get(
+              `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+            );
+            setDrink(response.data.drinks[0]);
+          } catch (e) {
+            setError("Failed to fetch drink details!");
+          }
+    
+          setIsLoading(false);
+        };
+    
+        getDrink();
+      }, [id]);
+    
+      if (isLoading) {
+        return <LoadingSpinner />;
+      }
+    
+      if (error) {
+        return <Error message={error} />;
+      }
+    
+      if (!drink) {
+        return null;
+      }
   return (
     <div>Cocktail</div>
   )
