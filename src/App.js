1b143872ad736d16b6ce81fb014ad6b1ebf92b4a
Crystal <muthonigachanja2000@gmail.com>
2023-08-04 12:14:51 -0700

    Using the switch and route

--- src/App.js
+++ src/App.js
@@ -2,3 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Cocktails from "./Cocktails";
 import Cocktail from "./Cocktail";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Cocktails} />
+        <Route path="/cocktails/:id" component={Cocktail} />
+        <Route path="/error">
+          <div>Error! No such page exists.</div>
+        </Route>
+        <Redirect to="/error" />
+      </Switch>
+    </Router>
+  );
+};
